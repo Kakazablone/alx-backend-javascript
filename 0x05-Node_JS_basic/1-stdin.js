3f0ad67e70295c4c87338e06fd8f377c71de50ce
Zablon Samba <zablonsamba@gmail.com>
2024-09-17 10:30:37 +0300

    Use Process stdin - commandline - to execute basic Javascript

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (data) => {
+  const input = data.toString().trim();
+  process.stdout.write(`Your name is: ${input}\n`);
+  process.exit();
+});
+
+process.on('exit', () => {
+  process.stdout.write('This important software is now closing\n');
+});
+
+module.exports = {}; // For testing purposes
